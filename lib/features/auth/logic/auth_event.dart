d26ddf0a0145fc44efe0e7f134ea201b0a5af7a1
luis fernando <luisrodriguuues@gmail.com>
2025-12-03 21:55:51 -0300

    auth_event manutençãoo

--- lib/features/auth/logic/auth_event.dart
+++ lib/features/auth/logic/auth_event.dart
@@ -1,34 +1,5 @@
 import 'package:equatable/equatable.dart';
 
-abstract class AuthEvent extends Equatable {
-  @override
-  List<Object?> get props => [];
-}
-
-class LoginRequested extends AuthEvent {
-  final String email;
-  final String password;
-
-  LoginRequested({required this.email, required this.password});
-
-  @override
-  List<Object?> get props => [email, password];
-}
-
-class RegisterRequested extends AuthEvent {
-  final String name;
-  final String email;
-  final String password;
-
-  RegisterRequested({
-    required this.name,
-    required this.email,
-    required this.password,
-  });
-
-  @override
-  List<Object?> get props => [name, emaiimport 'package:equatable/equatable.dart';
-
 /// Evento base para autenticação.
 abstract class AuthEvent extends Equatable {
   const AuthEvent();
@@ -77,7 +48,3 @@ class LogoutRequested extends AuthEvent {
   /// para manter consistência e extensibilidade.
   const LogoutRequested();
 }
-l, password];
-}
-
-class LogoutRequested extends AuthEvent {}
